e3bec5e780909fc42e43e7887b744043e2c42eb1
Steven Leyva <Sleyva1991@outlook.com>
2019-05-28 17:45:48 -0400

    Added controllers, models, and storyboard

---
+++ SwiftVocabulary/Controllers/VocabularyController.swift
@@ -0,0 +1,13 @@
+//
+//  VocabularyController.swift
+//  SwiftVocabulary
+//
+//  Created by Steven Leyva on 5/28/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class VocabularyController {
+    let vocabWords: [VocabularyWord] = []
+}
---
+++ SwiftVocabulary/Model/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Steven Leyva on 5/28/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
