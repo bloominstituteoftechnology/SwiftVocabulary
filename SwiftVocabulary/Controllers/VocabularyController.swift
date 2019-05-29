02ef6472e93c6008bd4f5713cbd2cc111c03b23d
Steven Leyva <Sleyva1991@outlook.com>
2019-05-28 20:35:24 -0400

    Add code to controllers to make app work

--- SwiftVocabulary/Controllers/VocabularyController.swift
+++ SwiftVocabulary/Controllers/VocabularyController.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 class VocabularyController {
-    let vocabWords: [VocabularyWord] = []
+//    let vocabWords: [VocabularyWord]
+//
+//    init() {
+//        self.vocabWords = [VocabularyWord(word: "hey", definition: "hello")]
+//
+    var vocabWords: [VocabularyWord] = []
+    
+    init() {
+        
+        createVocab(with: "Struct", definition: "Structures are general-purpose, flexible constructs that become the building blocks of your program’s code. You define properties and methods to add functionality to your structures and classes using the same syntax you use to define constants, variables, and functions.")
+    }
+    
+    func createVocab(with word: String, definition: String) {
+        let vocab = VocabularyWord(word: word, definition: definition)
+        
+        vocabWords.append(vocab)
+    }//    }
 }
