979ed4a9c5d657be3b26454de0e17e6c7af980cd
Taylor Lyles <hawks93187@gmail.com>
2019-07-15 13:52:21 -0700

    created view controllers and set up storyboard

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Taylor Lyles on 7/15/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+	@IBOutlet weak var definitionLabel: UILabel!
+	@IBOutlet weak var definitionTextView: UITextView!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
