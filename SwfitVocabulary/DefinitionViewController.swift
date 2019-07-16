aac0ca9abf66d119ae12082b2c4eed02e1b1c931
Nicolas Rios <nickrios33@yahoo.com>
2019-07-16 13:11:24 -0700

    save work

---
+++ SwfitVocabulary/DefinitionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DefinitionViewController.swift
+//  SwfitVocabulary
+//
+//  Created by Donella Barcelo on 7/15/19.
+//  Copyright © 2019 Donella Barcelo. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    var vocabWord: VocabularyWord?
+    
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateViews()
+      
+    }
+    
+    func  updateViews() {
+
+       let wordLabel + definitionTextView = vocabWord else { return }
+        if let vocabWord = vocabWord {
+            
+        }
+    
+    
+    
+        weak var wordLabel: UILabel!
+    
+        weak var definitionTextView: UITextView!
+    
+   
+    // MARK: - Navigation
+
+ /*   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDefinition" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+            let vocabWordsDetailVC = segue.destination as? DefinitionViewController
+        }
+*/
+    
+    
+    }
+
+
---
+++ SwfitVocabulary/VocabularyController.swift
@@ -0,0 +1,25 @@
+//
+//  VocabularyController.swift
+//  SwfitVocabulary
+//
+//  Created by Donella Barcelo on 7/15/19.
+//  Copyright © 2019 Donella Barcelo. All rights reserved.
+//
+
+import Foundation
+
+class VocabularyController {
+    var vocabWords: [VocabularyWord]
+    init()   {
+        vocabWords = [VocabularyWord(word: "Xcode", definition: """
+The integrated development environment (IDE) from Apple that is used to create, compile and test Mac OS X and iOS (iPhone/iPad/iPod) applications.
+""")]
+        vocabWords = [VocabularyWord(word: "Swift", definition: """
+Swift is a high-level programming language developed by Apple. It is designed for writing apps for Apple platforms, including macOS, iOS, tvOS, and watchOS.
+""")]
+        vocabWords = [VocabularyWord(word: "Constants", definition: """
+ Constants refer to fixed values that a program may not alter during its execution. Constants can be of any of the basic data types like an integer constant, a floating constant, a character constant, or a string literal.
+""")]
+        
+    }
+}
