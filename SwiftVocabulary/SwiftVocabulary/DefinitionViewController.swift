d3c6207aa954ab4953f2ec2262d93f2507a19fcf
JosephRogers94 <Joerogersss2016@gmail.com>
2019-07-19 17:51:17 -0500

    Add Scenes-Add View Controllers-Add Screens-Add Segues-Add Crucial Files

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Joseph Rogers on 7/19/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var definitionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,15 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Joseph Rogers on 7/19/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+
+import Foundation
+
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
