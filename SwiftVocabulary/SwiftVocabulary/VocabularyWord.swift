cc17df5fd23494d0b42ee1c0b0fe08ab9f90f83a
John Kouris <jdkouris@gmail.com>
2019-07-21 10:57:53 -0700

    Set up the interface with the storyboard Added the VocabularyWord struct Added the WordsTableViewController and the DefinitionTableViewController

---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,24 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by John Kouris on 7/21/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
+
+
+
+
+
+
+
+
+
+
