5629d5326391db127d3e8494e5e8d50051c9ea78
John Kouris <jdkouris@gmail.com>
2019-07-21 10:47:43 -0700

    Created new Xcode project SwiftVocabulary

---
+++ SwiftVocabulary/SwiftVocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftVocabulary
+//
+//  Created by John Kouris on 7/21/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
