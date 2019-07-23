cdeb751ec6076c814ba2fd7d5287b6c855dfcf46
Vici Shaweddy <vshaweddy@gmail.com>
2019-07-22 22:33:28 -0400

    adding detail view

---
+++ SwiftVocabulary/VocabularyController.swift
@@ -0,0 +1,21 @@
+//
+//  VocabularyController.swift
+//  SwiftVocabulary
+//
+//  Created by Vici Shaweddy on 7/22/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import Foundation
+
+class VocabularyController {
+    let vocabWords: [VocabularyWord]
+    
+    init() {
+        self.vocabWords = [
+            VocabularyWord(word: "Variable", definition: "To store and refer to values by an identifying name."),
+            VocabularyWord(word: "Constant", definition: "Variables whose values can’t be changed."),
+            VocabularyWord(word: "Function", definition: "Self-contained chunks of code that perform a specific task.")
+        ]
+    }
+}
---
+++ SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Vici Shaweddy on 7/22/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
