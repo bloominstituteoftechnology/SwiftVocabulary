613bb9bf118c341bbce65d44d5d52befd17b7883
Jessie Griffin <i.jessie.me@icloud.com>
2019-07-24 14:17:11 -0400

    make adjustments to the UI

--- SwiftVocabulary/View Controllers/WordsTableViewController.swift
+++ SwiftVocabulary/View Controllers/WordsTableViewController.swift
@@ -15,11 +15,7 @@ class WordsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        title = "Swift Vocabulary"
     }
 
     // MARK: - Table view data source
