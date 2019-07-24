f0db55ee9e38479385fb8b6efa530385ceb7fd93
FabiolaSaga <saga.fabiola@gmail.com>
2019-07-24 08:42:29 -0500

    Fnished MVP

---
+++ SwiftVocabulary/SwiftVocabulary/Model/VocabularyWord.swift
@@ -0,0 +1,16 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Fabiola S on 7/23/19.
+//  Copyright © 2019 Fabiola Saga. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+    
+}
