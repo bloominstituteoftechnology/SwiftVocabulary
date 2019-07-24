897ebee46dbb555dca64f837d297f1dfd2c8d375
dillonp23 <dillonp.lambda@gmail.com>
2019-07-23 20:55:52 -0700

    Created vocabulary word model struct

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Dillon P on 7/23/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
