910c389b9afd557df33afe505e3ce13ff5a8e390
dillonp23 <dillonp.lambda@gmail.com>
2019-07-23 20:54:18 -0700

    Added basic UI elements to Main.storyboard

---
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Dillon P on 7/23/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var wordDefinitionTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
