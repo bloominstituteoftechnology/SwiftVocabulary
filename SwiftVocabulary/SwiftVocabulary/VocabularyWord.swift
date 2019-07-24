fd29e8e41b820a225afe732d7c361485f3f2c320
KALISA FISHER <kiecreations@gmail.com>
2019-07-24 00:13:00 -0400

    Completed Main.Storyboard and VocabularyWord Model Struct Steps of Project

---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Lisa Fisher on 7/24/19.
+//  Copyright © 2019 Lisa Fisher. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
