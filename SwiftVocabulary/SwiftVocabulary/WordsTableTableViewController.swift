552a1713b3d730217e9e49cfcadc25342135921d
JosephRogers94 <Joerogersss2016@gmail.com>
2019-07-24 00:00:43 -0500

    uncomment line 72 to start working

--- SwiftVocabulary/SwiftVocabulary/WordsTableTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/WordsTableTableViewController.swift
@@ -47,46 +47,8 @@ class WordsTableTableViewController: UITableViewController {
     
     }
     
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    
     //MARK: - Navigation
-    
-    //     In a storyboard-based application, you will often want to do a little preparation before navigation
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         
@@ -95,8 +57,29 @@ class WordsTableTableViewController: UITableViewController {
         let definitionVC = segue.destination as? DefinitionViewController
         definitionVC?.vocabWord = vocabWordUsed
         
+    }
+    
+    
+    //MARK: - AddItems
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textfieldForName = UITextField()
+        var newWord : [VocabularyWord] = []
+        
+        let alert = UIAlertController(title: "Add new Vocabulary Word", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+//            self.vocabWord.append()
+        }
         
         
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "create name for word"
+            textfieldForName = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 }
