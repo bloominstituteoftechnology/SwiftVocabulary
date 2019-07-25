ff4bd04c609077755042057ce157fc09c4c8e838
Clayton Bonaguidi <claytonbonaguidi@gmail.com>
2019-07-25 13:29:05 -0700

    MVP Finished

--- Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
 
+    var vocabWord: VocabularyWord?
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var definitionTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let word = vocabWord {
+            updateViews(word: word)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews(word: VocabularyWord) {
+        
+        if let word = vocabWord {
+        wordLabel.text = word.word
+        definitionTextView.text = word.definition
+        }
     }
-    */
-
 }
---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,15 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Clayton Bonaguidi on 7/25/19.
+//  Copyright © 2019 Clayton Bonaguidi. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+    
+}
