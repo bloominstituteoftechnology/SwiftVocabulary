93ddc88e7b68904ff8f4533e11ad1a6f31de9a0b
bfvilchez <bfvilchez@icloud.com>
2019-08-12 14:11:50 -0600

    created a new xcode project

---
+++ SwiftVocabulary/SwiftVocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftVocabulary
+//
+//  Created by brian vilchez on 8/12/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
