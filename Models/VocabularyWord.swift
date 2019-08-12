9d3f21ea4dfd93f3fcf10e70db6a648a947d862f
Thomas Sabinowitz <53605757+RoastBeefKazenakis@users.noreply.github.com>
2019-08-12 19:48:33 -0400

    Couldn't commit changes via git for mysterious reasons

---
+++ Models/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Thomas Sabino-Benowitz on 8/12/19.
+//  Copyright © 2019 Thomas Sabino-Benowitz. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord{
+    let word: String
+    let definition: String
+}
