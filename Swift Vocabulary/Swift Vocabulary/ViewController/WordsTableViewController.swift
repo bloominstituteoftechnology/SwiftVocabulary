39c64a583259907df96f35119418a50d48bb5227
Percy Ngan <percival.ngan@gmail.com>
2019-08-12 18:25:46 -0500

    Added add button for additional words

--- Swift Vocabulary/Swift Vocabulary/ViewController/WordsTableViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/ViewController/WordsTableViewController.swift
@@ -14,9 +14,13 @@ class WordsTableViewController: UITableViewController {
 
 										VocabularyWord(word: "Class", definition: "A custom data type that can have one or more properties and one or more methods. Unlike structs, classes are reference types."),
 
-										VocabularyWord(word: "Class Inheritance", definition: "The ability for one class to build on another, inheriting all its methods and properties. Some languages allow one class to inherit from multiple parents, but Swift does not.")]
+										VocabularyWord(word: "Class Inheritance", definition: "The ability for one class to build on another, inheriting all its methods and properties. Some languages allow one class to inherit from multiple parents, but Swift does not.")
 
-    override func viewDidLoad() {
+										]
+
+
+
+	override func viewDidLoad() {
         super.viewDidLoad()
 
     }
@@ -46,43 +50,6 @@ class WordsTableViewController: UITableViewController {
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -99,6 +66,29 @@ class WordsTableViewController: UITableViewController {
 
 
     }
+
+	
+	@IBAction func addNewWord(_ sender: UIBarButtonItem) {
+
+		showAlertWithTextField()
+	}
+	
+	func showAlertWithTextField() {
+		let alertController = UIAlertController(title: "Add new tag", message: nil, preferredStyle: .alert)
+		let confirmAction = UIAlertAction(title: "Add", style: .default) { (_) in
+			if let txtField = alertController.textFields?.first, let text = txtField.text {
+				// operations
+				print("Text==>" + text)
+			}
+		}
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+		alertController.addTextField { (textField) in
+			textField.placeholder = "Tag"
+		}
+		alertController.addAction(confirmAction)
+		alertController.addAction(cancelAction)
+		self.present(alertController, animated: true, completion: nil)
+	}
 
 
 }
