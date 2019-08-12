5c987dc45756f8f8a1c9ee83047dd09e513adcf8
bfvilchez <bfvilchez@icloud.com>
2019-08-12 14:55:57 -0600

    finished tabelviewController

--- SwiftVocabulary/SwiftVocabulary/ViewControllers/VocabularyTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/ViewControllers/VocabularyTableViewController.swift
@@ -10,25 +10,45 @@ import UIKit
 
 class VocabularyTableViewController: UIViewController {
 
+    //MARK: - properties
+    var wordController = WordController()
     
+    //MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 //       UITableViewDelegate = self
 //        UITableViewDataSource = self
+//     self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowVocabularyDefinition" {
+          //  guard let definitionDetailVC = segue.destination as? VocabularyDefinitionViewController else {return}
+            
+        }
+    }
+    
+    
 }
 extension VocabularyTableViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return wordController.words.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+     let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+        let word = wordController.words[indexPath.row]
+        cell.textLabel?.text = word.word
+        return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            wordController.words.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
     
 }
