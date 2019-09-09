f09692bcf0148f6a4fff690d23691d458116aff1
Niranjan Kumar <njn.kumar@gmail.com>
2019-09-09 15:06:08 -0700

    working commit

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Niranjan Kumar on 9/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
