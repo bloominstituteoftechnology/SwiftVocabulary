45aa6b60dd89f5592a0be150eb49ffec8053f809
Jesse Ruiz <jesseleeruiz@Jesses-Air.hawaii.rr.com>
2019-09-10 10:04:47 -1000

    Finished MVP

---
+++ SwiftVocabulary1/SwiftVocabulary1/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Jesse Ruiz on 9/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
---
+++ SwiftVocabulary1/SwiftVocabulary1/WordsTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  WordsTableViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Jesse Ruiz on 9/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class WordsTableViewController: UITableViewController {
+    
+    var vocabWords: [VocabularyWord] = [VocabularyWord(word: "Variable", definition: "A named value used to store information."),
+                                        VocabularyWord(word: "Constant", definition: "Constants refer to fixed values that a program may not alter during its execution. Constants can be of any of the basic data types like an integer constant, a floating constant, a character constant, or a string literal."),
+                                        VocabularyWord(word: "Function", definition: "Functions are self-contained chunks of code that perform a specific task. You give a function a name that identifies what it does, and this name is used to “call” the function to perform its task when needed.")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return vocabWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+        
+        // Configure the cell...
+        
+        let vocabularyWord = vocabWords[indexPath.row]
+        cell.textLabel?.text = vocabularyWord.word
+        
+        return cell
+    }
+    
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let indexPath = tableView.indexPathForSelectedRow,
+            let definitionVC = segue.destination as? DefinitionViewController else { return }
+        
+        definitionVC.vocabWord = vocabWords[indexPath.row]
+    }
+    
+    
+}
