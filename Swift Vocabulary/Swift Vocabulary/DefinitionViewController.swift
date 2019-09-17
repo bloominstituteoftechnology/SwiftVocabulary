e0644425524e85115618ad0a3ef5b2773dbac961
CrystalRainn <CrystalRainn@icloud.com>
2019-09-17 00:17:20 -0400

    Created Swift Vocabulary project and set up NavigationController, WordsTableViewController, and DefinitionViewController.

---
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Christy Hicks on 9/16/19.
+//  Copyright © 2019 Rainn Inc. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    @IBOutlet var TermLabel: UILabel!
+    
+    @IBOutlet var DefinitionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
