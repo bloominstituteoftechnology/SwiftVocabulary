8df842bdd6afbf2834ca5978d19fe871a076ab5b
Nelson Gonzalez <nelglezfl@gmail.com>
2019-09-18 11:46:43 -0400

    Finished app

--- Swift Vocabulary/Swift Vocabulary/View Controllers/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/View Controllers/DefinitionViewController.swift
@@ -12,11 +12,18 @@ class DefinitionViewController: UIViewController {
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var definitionTextView: UITextView!
     
+    var vocaWord: VocabularyWord?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateViews()
     }
 
+    private func updateViews() {
+        guard let vocaWord = vocaWord else { return }
+        wordLabel.text = vocaWord.word
+        definitionTextView.text = vocaWord.definition
+    }
 
 }
 
--- Swift Vocabulary/Swift Vocabulary/View Controllers/WordsTableViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/View Controllers/WordsTableViewController.swift
@@ -38,17 +38,17 @@ class WordsTableViewController: UITableViewController {
 
         return cell
     }
-    
-
-   
-
    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ToShowDefinitionVC" {
+            if let destination = segue.destination as? DefinitionViewController {
+                guard let indexPath = tableView.indexPathForSelectedRow else { return }
+                destination.vocaWord = vocabWords[indexPath.row]
+            }
+        }
     }
    
 
