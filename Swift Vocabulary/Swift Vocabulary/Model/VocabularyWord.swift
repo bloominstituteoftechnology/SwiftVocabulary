26cefd83d41c7e81415e267a7b4523d14d741e94
Nelson Gonzalez <nelglezfl@gmail.com>
2019-09-18 11:29:46 -0400

    Added model struct and re-organized files into groups.

---
+++ Swift Vocabulary/Swift Vocabulary/Model/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Nelson Gonzalez on 9/18/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
