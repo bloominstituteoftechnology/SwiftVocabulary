78e522c3077d86a7114f4b6fc68dd56796375171
Craig Swanson <cswanson02@me.com>
2019-09-18 12:46:04 -0500

    Added table view controller and view controller

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Craig Swanson on 9/18/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var wordToDefine: UILabel!
+    @IBOutlet weak var wordDefinition: UITextView!
+    
+    // testing
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Craig Swanson on 9/18/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
