c983786db5c27721b5be33264b265b495389b0d6
Nicolas  Rios <nickrios33@yahoo.com>
2019-09-18 15:27:02 -0700

    updated autolayout issue

--- SwfitVocabulary/VocabularyController.swift
+++ SwfitVocabulary/VocabularyController.swift
@@ -22,7 +22,7 @@ The integrated development environment (IDE) from Apple that is used to create, 
 """), VocabularyWord(word: "Swift", definition: """
     Swift is a high-level programming language developed by Apple. It is designed for writing apps for Apple platforms, including macOS, iOS, tvOS, and watchOS.
     """), VocabularyWord(word: "Constants", definition: """
-    Constants refer to fixed values that a program may not alter during its execution. Constants can be of any of the basic data types like an integer constant, a floating constant, a character constant, or a string literal.
+       Constants refer to fixed values that a program may not alter during its execution. Constants can be of any of the basic data types like an integer constant, a floating constant, a character constant, or a string literal.
     """), VocabularyWord(word: "apple", definition: "A Computer company")]
     }
 }
