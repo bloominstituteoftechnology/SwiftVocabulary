80882704c5f6b515206533f56a2c696f7a399774
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-09-18 21:59:42 -0400

    Implement WordsTableViewController

--- Swift Vocabulary/Swift Vocabulary/WordsTableViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/WordsTableViewController.swift
@@ -23,24 +23,23 @@ class WordsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return vocabWords.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+        let word = vocabWords[indexPath.row]
+        cell.textLabel?.text = word?.word
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -77,14 +76,24 @@ class WordsTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
+        if segue.identifier == "ShowDefinitionSegue" {
+            guard let detailVC = segue.destination as? DefinitionViewController, let indexPath = tableView.indexPathForSelectedRow else { return }
+            let word = vocabWords[indexPath.row]
+            detailVC.title = word?.word
+            
+        }
+    }        // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
-    */
-
+   
+    
+    
+    
 }
+
+// Variables
+var vocabWords: [VocabularyWord?] = [nil]
