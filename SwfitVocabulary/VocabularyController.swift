4a85d811abddb511d56aff9fc127e489a24d8b54
Nicolas  Rios <nickrios33@yahoo.com>
2019-09-18 18:35:18 -0700

    fixed definition

--- SwfitVocabulary/VocabularyController.swift
+++ SwfitVocabulary/VocabularyController.swift
@@ -23,6 +23,6 @@ The integrated development environment (IDE) from Apple that is used to create, 
     Swift is a high-level programming language developed by Apple. It is designed for writing apps for Apple platforms, including macOS, iOS, tvOS, and watchOS.
     """), VocabularyWord(word: "Constants", definition: """
        Constants refer to fixed values that a program may not alter during its execution. Constants can be of any of the basic data types like an integer constant, a floating constant, a character constant, or a string literal.
-    """), VocabularyWord(word: "apple", definition: "A Computer company")]
+    """), VocabularyWord(word: "Apple", definition: "Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services.")]
     }
 }
