3bacaddb55b0226433e53a1868882f3640e0cc85
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-18 22:58:06 -0400

    cleaned // code

--- Swift Vocabulary/Swift Vocabulary/View Controllers/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/View Controllers/DefinitionViewController.swift
@@ -15,27 +15,15 @@ class DefinitionViewController: UIViewController {
     @IBOutlet weak var definitionViewTextView: UITextView!
     
     func updateViews(){
-        if let vocabWords = vocabWord{
-            self.definitionViewTextLabel.text = vocabWords.word
-            self.definitionViewTextView.text = vocabWords.definition
-        }
+        guard let vocabWords = vocabWord else{return}
+        definitionViewTextLabel.text = vocabWords.word
+        definitionViewTextView.text = vocabWords.definition
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
