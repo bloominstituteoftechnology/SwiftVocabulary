fca6564fe2aed1bc76e2a260d2a5db04dd04648f
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-09-18 21:08:47 -0400

    Added VocabularyWord file and struct.

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Christy Hicks on 9/18/19.
+//  Copyright © 2019 Rainn Inc. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
