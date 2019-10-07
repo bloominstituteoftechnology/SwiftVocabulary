6e44d12827638d9b16da60114ee0134b42af3a69
Jon Bash <jonbash@protonmail.com>
2019-10-07 13:45:47 -0700

    made AddWordSegue work

--- Swift Vocabulary/Swift Vocabulary/AddWordViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/AddWordViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class AddWordViewController: UIViewController {
+    
+    var word: String = ""
+    var definition: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func wordTextChanged(_ sender: UITextField) {
+        word = sender.text ?? ""
     }
-    */
-
+    
+    @IBAction func definitionTextChanged(_ sender: UITextField) {
+        definition = sender.text ?? ""
+    }
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
 }
