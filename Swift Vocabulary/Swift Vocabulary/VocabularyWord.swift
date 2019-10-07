8625f053756e29680778b9d733fa4a0511addaba
Dan M <djmthedev@gmail.com>
2019-10-07 13:09:01 -0700

    added loadFromFile to VocabularyWord

--- Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -26,19 +26,19 @@ struct VocabularyWord: Codable {
         try? encodedVocabularyWords?.write(to: VocabularyWord.ArchiveURL, options: .noFileProtection)
     }
     
-//    static func loadFromFile() -> [VocabularyWord] {
-//
-//        let propertyListDecoder = PropertyListDecoder()
-//
-//        if let retrievedVocabularyWordData = try? Data(contentsOf: VocabularyWord.ArchiveURL) {
-//            let decodedVocabularyWords = try? propertyListDecoder.decode([VocabularyWord].self, from: retrievedVocabularyWordData)
-//            guard let vocabularyWords = decodedVocabularyWords else { return loadSampleVocabularyWords() }
-//
-//            return vocabularyWords
-//        } else {
-//            return loadSampleVocabularyWords()
-//        }
-//    }
+    static func loadFromFile() -> [VocabularyWord] {
+
+        let propertyListDecoder = PropertyListDecoder()
+
+        if let retrievedVocabularyWordData = try? Data(contentsOf: VocabularyWord.ArchiveURL) {
+            let decodedVocabularyWords = try? propertyListDecoder.decode([VocabularyWord].self, from: retrievedVocabularyWordData)
+            guard let vocabularyWords = decodedVocabularyWords else { return loadSampleVocabularyWords() }
+
+            return vocabularyWords
+        } else {
+            return loadSampleVocabularyWords()
+        }
+    }
     
 //    static func loadSampleVocabularyWords () -> [VocabularyWord] {
 //        let vocabWords: [VocabularyWord] = [VocabularyWord(word: "Abstraction", definition: "An abstraction groups things together, so you can think of the group as a single, easier-to-remember thing instead of trying to reason about each of the elements separately. Abstraction allows programmers to make decisions and think through code logic without getting overwhelmed by details and complexity."), VocabularyWord(word: "Accessibility", definition: "Accessibility features on iOS include VoiceOver, Zoom, Invert Colors, and more. These features improve the experience for users with particular preferences or needs, possibly stemming from visual, audible, or motor impairments."), VocabularyWord(word: "Accessory View", definition: "Table view cells have an accessory view that can be used to display additional information.")]
