905cc5d81371dac3ae61e4a4c69de3f45aa0c6d7
Rwoltx <richardx1016@gmail.com>
2019-10-07 21:40:53 -0400

    Completed the UI and DefinitionViewController

---
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Rick Wolter on 10/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    
+    
+    var vocabWord: VocabularyWord?
+    
+    @IBOutlet var wordLabel: UILabel!
+    
+    @IBOutlet var definitionTextView: UITextView!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    
+    func updateViews(){
+        
+        if let vocabWord = vocabWord {
+            wordLabel.text = vocabWord.word
+            definitionTextView.text = vocabWord.definition
+        }
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
