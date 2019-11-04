6e070363e9eae886e6f4de7b5bfa273686b7c91b
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-04 21:59:00 +0000

    The first four sections of the project complete

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Tobi Kuyoro on 04/11/2019.
+//  Copyright © 2019 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var viewLabel: UILabel!
+    @IBOutlet weak var viewText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
