c1545a6372bc977184fce5d826230fdca24aadd9
David Wright <davidtwright@gmail.com>
2019-11-13 02:28:57 -0500

    Created a VocabularyWord struct and implemented WordsTableViewController

--- Swift Vocabulary/WordsTableViewController.swift
+++ Swift Vocabulary/WordsTableViewController.swift
@@ -10,37 +10,27 @@ import UIKit
 
 class WordsTableViewController: UITableViewController {
 
+    var vocabWords: [VocabularyWord] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+ 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return vocabWords.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+        
+        cell.textLabel?.text = vocabWords[indexPath.row].word
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -62,29 +52,18 @@ class WordsTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDefinitionSegue" {
+            guard let cell = tableView.indexPathForSelectedRow, let definitionVC = segue.destination as? DefinitionViewController else { return }
+            
+            // Pass the selected object to the new view controller.
+            definitionVC.wordLabel.text = vocabWords[cell.row].word
+        }
     }
-    */
-
+    
 }
