1a233be2e0549d08734db2d238606d524f0ff62a
David Wright <davidtwright@gmail.com>
2019-11-13 01:37:46 -0500

    Created view controllers and added screens to Main.storyboard

---
+++ Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by David Wright on 11/13/19.
+//  Copyright © 2019 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet var wordLabel: UILabel!
+    @IBOutlet var definitionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
