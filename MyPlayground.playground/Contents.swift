852cf5cea42f636fb0ad9831ec889afc5cce6d4c
alfcolon <alfcolon@gmail.com>
2019-11-14 15:16:14 -0800

    removing files

--- MyPlayground.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-var str = "Hello, playground"
-let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
-   alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-   NSLog("The \"OK\" alert occured.")
-   }))
