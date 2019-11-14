9fff5ffed54e3287b7ee86aaba33ebe0248c9ae2
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-11-13 21:36:45 -0500

    Created VocabularyWord.swift

---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Joshua Rutkowski on 11/13/19.
+//  Copyright © 2019 Joshua Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
