b6743fc896cf5f288107a6a3b296e3b1efb05b92
Jeremy Taylor <jtaylor@jctaylor.org>
2019-11-13 20:56:30 -0600

    Implemented DefinitionViewController

--- Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
-    @IBOutlet weak var wordLebel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var definitionTextView: UITextView!
     
     var vocabWord: VocabularyWord?
@@ -17,18 +17,13 @@ class DefinitionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard isViewLoaded,
+        let entry = vocabWord else { return }
+        wordLabel.text = entry.word
+        definitionTextView.text = entry.definition
     }
-    */
-
 }
