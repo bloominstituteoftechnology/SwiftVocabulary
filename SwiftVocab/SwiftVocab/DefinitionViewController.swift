57fbd173f536f3a6985fcd516e9d5f9cea4a8575
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-09 12:55:37 -0800

    created view controllers

---
+++ SwiftVocab/SwiftVocab/DefinitionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocab
+//
+//  Created by Jorge Alvarez on 12/9/19.
+//  Copyright © 2019 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
