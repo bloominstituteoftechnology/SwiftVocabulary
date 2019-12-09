feed2232fd26ad33cce3f0f09ae414fa10b670ce
Tobi Tselios <tobi.tselios@gmail.com>
2019-12-09 21:23:33 +0000

    View controllers created and screens added to storyboard

---
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Tobi Kuyoro on 09/12/2019.
+//  Copyright © 2019 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var definitionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift Vocabulary/Swift Vocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Tobi Kuyoro on 09/12/2019.
+//  Copyright © 2019 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
