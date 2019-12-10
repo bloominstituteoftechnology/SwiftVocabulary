2187e2b1254da563de46fd6b12b215a8c98f9148
Michael McGrath <maybemichael@icloud.com>
2019-12-09 18:22:59 -0800

    iOS Fundamentals Afternoon Project w/ Stretch

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,16 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Michael on 12/9/19.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
+
+
