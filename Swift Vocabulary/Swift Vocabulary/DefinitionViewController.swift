d799449a9fcf7dc5e98c3e3ca40ae3f1c20bbac3
Samuel Esserman <samessermanlambda@gmail.com>
2020-01-18 10:14:55 -0500

    This is Monday's project with all questions answered besides the SGs.

--- Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -9,29 +9,24 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
-
     
+    var vocabWord: VocabularyWord?
+
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textView: UITextView!
     
 
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard let unwrappedVocabWord = vocabWord else { return }
+        label.text = unwrappedVocabWord.word
+        textView.text = unwrappedVocabWord.definition
     }
-    */
+
 
 }
