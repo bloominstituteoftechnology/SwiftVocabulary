2def6250038d727101855c6cd022f16c1c0e1b17
Jarren Campos <jarrenmcampos@gmail.com>
2020-01-26 14:45:59 -0800

    Project Setup

---
+++ Swift Vocabulary/Swift Vocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Jarren Campos on 1/26/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
