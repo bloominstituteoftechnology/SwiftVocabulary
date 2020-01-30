cc7b95d69a75e7f4decd76c5d0042fc172dd0bb6
Jorge Talavera <jorgetalavera02@gmail.com>
2020-01-29 18:29:54 -0800

    Added my name to test variable

--- VocabularyApp/VocabularyApp/DefinitionViewController.swift
+++ VocabularyApp/VocabularyApp/DefinitionViewController.swift
@@ -16,6 +16,7 @@ class DefinitionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
 
     /*
     // MARK: - Navigation
--- VocabularyApp/VocabularyApp/WordsTable.swift
+++ VocabularyApp/VocabularyApp/WordsTable.swift
@@ -11,3 +11,5 @@ import UIKit
 class WordsTable: UItableViewController {
 
 }
+
+let name = "Jorge Talavera"
