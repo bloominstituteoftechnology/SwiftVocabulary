f55665e6043f0349334671fbb5842fe8e6e11082
Christopher DeVito <toolofjesus@protonmail.com>
2020-01-29 19:06:44 -0500

    Initial commmit with views set up

---
+++ SwiftVocabulary/SwiftVocabulary/ViewControllers/DefinitionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Lambda_School_Loaner_259 on 1/29/20.
+//  Copyright © 2020 DeVitoC. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
