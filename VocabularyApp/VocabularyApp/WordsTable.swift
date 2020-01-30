531cba564a7bec58ee838f4ccc899f4804e2a5f2
Jorge Talavera <jorgetalavera02@gmail.com>
2020-01-29 17:12:22 -0800

    Added 2 Cocotouch View Controllers

---
+++ VocabularyApp/VocabularyApp/WordsTable.swift
@@ -0,0 +1,13 @@
+//
+//  WordsTable.swift
+//  VocabularyApp
+//
+//  Created by Jorge Talavera on 1/29/20.
+//  Copyright © 2020 Jorge Talavera. All rights reserved.
+//
+
+import UIKit
+
+class WordsTable: UItableViewController {
+
+}
