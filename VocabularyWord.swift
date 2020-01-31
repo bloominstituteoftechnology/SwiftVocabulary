e36ae0191e797bb0e895ea71e0534e4392f214db
hai666l <0xconr@protonmail.com>
2020-01-30 19:04:48 -0600

    Created a VocabularyWord Model Struct

---
+++ VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by conner on 1/29/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
