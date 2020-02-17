2496c788904651f801668dff9dc41ab7a49bb8e0
Diego Covarrubias <dcov@pm.me>
2020-02-17 13:55:21 -0800

    Add ViewControllers, and storyboard

--- Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
 
+    @IBOutlet weak var wordLabel: UILabel!
+    
+    @IBOutlet weak var definitionTextView: UITextView!
+    
+    var vocabWord: VocabularyWord?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard let vocabWord = self.vocabWord else {
+            return
+        }
+        
+        wordLabel.text = vocabWord.word
+        definitionTextView.text = vocabWord.definition
     }
-    */
-
 }
---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,16 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by dc on 2/17/20.
+//  Copyright © 2020 Diego Covarrubias. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    
+    let word: String
+    
+    let definition: String
+}
