3b0cc65f8f563771baab31c8b57bd223e6414d89
Mark Gerrior <github@gerrior.com>
2020-02-17 14:35:52 -0800

    Create a VocabularyWord Model Struct

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Mark Gerrior on 2/17/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
