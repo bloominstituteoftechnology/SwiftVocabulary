126ca656a35fcd1321bfef1095c676dfb8d18cb8
Hector L <hectoremil21@hotmail.com>
2020-02-17 17:10:56 -0500

    Copy-pasted UIAlertController code triggers an alert succesfully.

--- SwiftVocabulary/SwiftVocabulary/WordsTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/WordsTableViewController.swift
@@ -21,7 +21,11 @@ class WordsTableViewController: UITableViewController {
     
     @IBAction func addWordButtonPress(_ sender: Any) {
         vocabWords[0] = VocabularyWord(word: "Button", definition: "was pressed")
-        self.reloadInputViews()
+        let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
         
     }
     
