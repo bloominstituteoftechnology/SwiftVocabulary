1fc1efd185a5d9a5be2656acdcef277db9a0444e
Shawn Gee <shawngee@gmail.com>
2020-02-17 18:37:03 -0500

    Implemented alphabet index and section titles

--- SwiftVocabulary/SwiftVocabulary/Controller/WordsTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/Controller/WordsTableViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class WordsTableViewController: UITableViewController {
 
-    var vocabWords = [VocabularyWord]()
+    var vocabIndex = [String: [VocabularyWord]]()
+    var sectionTitles = [String]()
+    
     let webScraper =  WebScraper()
     
     let activityIndicatorView = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupActivityIndicator()
+        fetchVocabWords()
+    }
+
+    func setupActivityIndicator() {
         self.view.addSubview(activityIndicatorView)
         activityIndicatorView
             .centerHorizontally()
@@ -24,40 +31,68 @@ class WordsTableViewController: UITableViewController {
             .constrain(width: 200, height: 200)
         activityIndicatorView.startAnimating()
         activityIndicatorView.style = .large
-        
-        fetchVocabWords()
-        
     }
-    
     
     func fetchVocabWords() {
         webScraper.fetchVocabWords { [weak self] vocabWords in
             guard let self = self else { return }
             DispatchQueue.main.async {
-                self.vocabWords = vocabWords
+                self.buildVocabIndex(withVocabWords: vocabWords)
                 self.tableView.reloadData()
                 self.activityIndicatorView.stopAnimating()
             }
         }
     }
+    
+    func buildVocabIndex(withVocabWords vocabWords: [VocabularyWord]) {
+        for vocabWord in vocabWords {
+            let key = String(vocabWord.word.prefix(1))
+            
+            if var value = vocabIndex[key] {
+                value.append(vocabWord)
+                vocabIndex[key] = value
+            } else {
+                vocabIndex[key] = [vocabWord]
+            }
+        }
+        
+        sectionTitles = [String](vocabIndex.keys)
+        sectionTitles.sort { $0 < $1 }
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        sectionTitles.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        vocabWords.count
+        let key = sectionTitles[section]
+        if let values = vocabIndex[key] {
+            return values.count
+        }
+        
+        return 0
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
-        let vocabWord = vocabWords[indexPath.row]
-        cell.textLabel?.text = vocabWord.word
-
+        
+        let key = sectionTitles[indexPath.section]
+        if let values = vocabIndex[key] {
+            cell.textLabel?.text = values[indexPath.row].word
+        }
+        
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        sectionTitles[section]
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        sectionTitles
     }
 
     // MARK: - Navigation
@@ -66,9 +101,11 @@ class WordsTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowDefinitionSegue",
            let definitionViewController = segue.destination as? DefinitionViewController,
-           let selectedRow = tableView.indexPathForSelectedRow?.row
-        {
-            definitionViewController.vocabWord = vocabWords[selectedRow]
+           let selectedIndex = tableView.indexPathForSelectedRow {
+            let key = sectionTitles[selectedIndex.section]
+            if let values = vocabIndex[key] {
+                definitionViewController.vocabWord = values[selectedIndex.row]
+            }
         }
     }
 
