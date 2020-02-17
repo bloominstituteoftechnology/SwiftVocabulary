47f203a08852679c63aff145b33af9a37b574a0d
Shawn-James <shawn.james@me.com>
2020-02-17 17:14:26 -0500

    project file added

---
+++ SwiftVocabulary/SwiftVocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Shawn James on 2/17/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
