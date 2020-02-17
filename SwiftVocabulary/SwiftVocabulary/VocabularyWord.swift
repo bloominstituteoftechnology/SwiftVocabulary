13e6c74c45f4f7ce4cad5618b9ad7b70040cd03e
Hector L <hectoremil21@hotmail.com>
2020-02-17 13:11:24 -0500

    Added VocabularyWord.swift and the struct

---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Karen Rodriguez on 2/17/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
