70bd44103569dd77a14b75050ca19006bbc88074
Richard Ray Wellman <trys10studios@gmail.com>
2020-02-17 15:59:49 -0600

    up to vocabword struct

--- Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -1,9 +1,16 @@
 //
-//  File.swift
+//  VocabularyWord.swift
 //  Swift Vocabulary
 //
 //  Created by Ray Wellman on 2/17/20.
 //  Copyright © 2020 Richard Ray Wellman. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+// A struct for a word and its definition
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
+
