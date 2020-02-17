847e7b450e445007d94381623c7873b9a99622e2
Hector L <hectoremil21@hotmail.com>
2020-02-17 13:28:00 -0500

    Implemented cellForRowAt in WordsTableViewController

--- SwiftVocabulary/SwiftVocabulary/WordsTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/WordsTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class WordsTableViewController: UITableViewController {
 
+    var vocabWords: [VocabularyWord] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,25 +24,26 @@ class WordsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return vocabWords.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+        
+        let word = vocabWords[indexPath.row]
+        
+        cell.textLabel?.text = word.word
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
