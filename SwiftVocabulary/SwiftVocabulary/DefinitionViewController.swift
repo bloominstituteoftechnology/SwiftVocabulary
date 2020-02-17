bda83da1ef0b313d6e5271271fd77b17e1ee6b42
fthmls <fthmls.one@gmail.com>
2020-02-17 15:40:00 -0500

    Layout Storyboard

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by fthmls on 2/17/20.
+//  Copyright © 2020 fthmls. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var labelView: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
