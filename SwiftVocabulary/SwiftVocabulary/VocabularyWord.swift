0da6ccebbf588e10e40bd0a75ebb1bcec5697119
Harmony Radley <harmonyradley99@gmail.com>
2020-02-17 20:15:51 -0500

    recreating VocabularyWord model struct

---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,15 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Harmony Radley on 2/17/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import Foundation
+
+
+struct VocabularyWord {
+    let variable: String
+    let definiton: String
+}
