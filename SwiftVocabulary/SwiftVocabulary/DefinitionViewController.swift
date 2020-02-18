4b00d22d8af444399a9d60c593fe171436707ac6
Harmony Radley <harmonyradley99@gmail.com>
2020-02-17 20:20:15 -0500

    DefinitionViewController and WordsTableController

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Harmony Radley on 2/17/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+
+class DefinitonViewController: UIViewController {
+
+    var vocabWord: VocabularyWord?
+    func updatesViews() {
+        if let unwrappedWord = vocabWord {
+            print(unwrappedWord)
+            variable.text = unwrappedWord.variable
+            variableDefiniton.text = unwrappedWord.definiton
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updatesViews()
+        
+    }
+    
+
+    @IBOutlet weak var variable: UILabel!
+    @IBOutlet weak var variableDefiniton: UITextView!
+
+}
