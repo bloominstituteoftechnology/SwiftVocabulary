ca24eea73886a6a6b9719753185304f2a1367bea
Breanna Young <bree.j.32@gmail.com>
2020-02-26 13:39:25 -0500

    implemented words tableviewcontroller and definition view controller

--- Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -9,24 +9,26 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
-
+    
     @IBOutlet weak var definitionVC: UILabel!
-    @IBOutlet weak var textViewVC: UITextView!
+    var vocabWord: VocabularyWord?
+
+    @IBOutlet weak var definitionTextVC: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateViews()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   func updateViews() {
+    if let vocabWord = vocabWord {
+        
+        definitionVC.text = vocabWord.word
+        definitionTextVC.text = vocabWord.definition
+            
     }
-    */
+    }
+    
 
 }
--- Swift Vocabulary/Swift Vocabulary/WordsTableViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/WordsTableViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class WordsTableViewController: UITableViewController {
+    
+    var vocabWords: [VocabularyWord] = [
+        VocabularyWord(word: "Array", definition: "A sequential collection of values of any type, such as an array of names in a band."),
+        VocabularyWord(word: "Boolean", definition: "A data type that stores either true or false."),
+        VocabularyWord(word: "Class", definition: "A custom data type that can have one or more properties and one or more methods. Unlike structs, classes are reference types."),
+        VocabularyWord(word: "Data", definition: "A type that holds any kind of binary data."),
+        VocabularyWord(word: "Enum", definition: "A set of named values that are easier to remember and safer than just using strings or integers. For example, you might create an enum of directions, using north, south, east, and west – this is much nicer than using 0, 1, 2, and 3, for example. Short for enumeration.")
+        
+    ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
+ 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -28,63 +28,29 @@ class WordsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+       
+        return vocabWords.count
     }
+    
 
-    /*
+   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShowDefinitionSegue", for: indexPath)
+        
+        let vocabWord = vocabWords[indexPath.row]
+        cell.textLabel?.text = vocabWord.word
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        if segue.identifier == "ShowDefinitionSegue" {
+                if let definitionVC = segue.destination as? DefinitionViewController,
+                    let indexPath = tableView.indexPathForSelectedRow {
+                    let vocabWord = vocabWords[indexPath.row]
+                    definitionVC.vocabWord = vocabWord
+                }
+            }
+        
+        }
 }
