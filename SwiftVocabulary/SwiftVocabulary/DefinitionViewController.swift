0964e8fd25ec2aa5edb6cf41a19aa6583df12d3c
Shawn-James <shawn.james@me.com>
2020-03-16 19:03:52 -0400

    remove unused comments

--- SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -29,15 +29,5 @@ class DefinitionViewController: UIViewController {
         wordLabel.text = vocabWordUnwrapped.word
         definitionTextView.text = vocabWordUnwrapped.definition
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
