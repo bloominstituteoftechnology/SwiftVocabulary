ff25b8f5b4b5bcc94584b2de1a5aa7dd383ec04c
Cameron Collins <cameroncollins19@yahoo.com>
2020-03-16 14:36:46 -0500

    Added File's and Storyboard Set up

---
+++ Swift Vocabulary/Swift Vocabulary/Words.swift
@@ -0,0 +1,14 @@
+//
+//  Words.swift
+//  Swift Vocabulary
+//
+//  Created by Cameron Collins on 3/16/20.
+//  Copyright © 2020 Cameron Collins. All rights reserved.
+//
+
+import Foundation
+
+struct Word {
+    var word: String
+    var definition: String
+}
