6e9dc4723f1452b20eb0fe82b5d6d14b5d31d1d3
Hunter Oppel <hun.oppel@gmail.com>
2020-03-16 18:13:34 -0400

    Added finished project plus stretch goal.

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Hunter Oppel on 3/16/20.
+//  Copyright © 2020 LambdaSchool. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
