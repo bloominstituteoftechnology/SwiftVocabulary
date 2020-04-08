edea1cdc05972f6928ed758951be68b864c9f655
Aaron Peterson <aarondp2011@gmail.com>
2020-04-08 13:07:29 -0500

    Added outlets to Definition View Controller

--- Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
 
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var definitionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
