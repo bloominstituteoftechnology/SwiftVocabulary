a76d285a4e3ccf84cab44d4497b6b9b6ac969abd
Aaron Peterson <aarondp2011@gmail.com>
2020-04-08 13:10:26 -0500

    Created VocabularyWord struct

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Aaron Peterson on 4/8/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
