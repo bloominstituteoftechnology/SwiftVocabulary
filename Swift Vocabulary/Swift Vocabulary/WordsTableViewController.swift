cb802b1beda1ad2b5c39929a3637c63ee74ffca2
Aaron Peterson <aarondp2011@gmail.com>
2020-04-08 13:28:15 -0500

    Implemented table view protocols

--- Swift Vocabulary/Swift Vocabulary/WordsTableViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/WordsTableViewController.swift
@@ -9,38 +9,32 @@
 import UIKit
 
 class WordsTableViewController: UITableViewController {
+    
+    var vocabWords: [VocabularyWord] = [
+        VocabularyWord(word: "Variable", definition: "A named value used to store infomation that can be changed after being created."),
+        VocabularyWord(word: "Constant", definition: "A named value used to store infomation that cannot be changed after being created."),
+        VocabularyWord(word: "Function", definition: "A self contained chunck of code that performs a specific task.")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return vocabWords.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
 
-        // Configure the cell...
+        let vocabWord = vocabWords[indexPath.row]
+        cell.textLabel?.text = vocabWord.word
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
