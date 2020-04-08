b8acb480b19f0f1d677afc565cc74ea8622f257f
Clayton Watkins <watkinclayton17@yahoo.com>
2020-04-08 11:04:22 -0700

    Set up storyboard and view controller classes

---
+++ Swift Vocabulary/Swift Vocabulary/Model/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Swift Vocabulary
+//
+//  Created by Clayton Watkins on 4/8/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+
---
+++ Swift Vocabulary/Swift Vocabulary/ViewController/DefinitonViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DefinitonViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Clayton Watkins on 4/8/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class DefinitonViewController: UIViewController {
+
+    @IBOutlet weak var definitionTextField: UITextView!
+    @IBOutlet weak var definitionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
