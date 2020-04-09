c751ed5bcb214b4db2d41a78adbdcd6fe6c3c53c
Ian French <igf2011@hotmail.com>
2020-04-09 12:48:50 -0400

    Final cleaned up copy migrated from Swift-Vocabulary repository

--- Vocabulary/Swift Vocabulary/Swift Vocabulary/View Controllers/DefinitionViewController.swift
+++ Vocabulary/Swift Vocabulary/Swift Vocabulary/View Controllers/DefinitionViewController.swift
@@ -23,7 +23,7 @@ class DefinitionViewController: UIViewController {
         
             updateViews()
         
-        // Do any additional setup after loading the view.
+      
     }
     
     
@@ -37,14 +37,5 @@ class DefinitionViewController: UIViewController {
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 
