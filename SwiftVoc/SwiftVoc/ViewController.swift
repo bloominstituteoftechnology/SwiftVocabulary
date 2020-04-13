70bc9ca790da060c792ce72410674c44664117b0
cedric janssens <cedric-janssens@lambdastudent.com>
2020-04-13 19:14:04 -0400

    1.1

---
+++ SwiftVoc/SwiftVoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftVoc
+//
+//  Created by Cedric Janssens on 4/13/20.
+//  Copyright © 2020 Cedric Janssens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftVoc/SwiftVoc/VocabularyWord.swift
@@ -0,0 +1,19 @@
+//
+//  VocabularyWord.swift
+//  SwiftVoc
+//
+//  Created by Cedric Janssens on 4/13/20.
+//  Copyright © 2020 Cedric Janssens. All rights reserved.
+//
+
+import UIKit
+
+
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
+
+
+
