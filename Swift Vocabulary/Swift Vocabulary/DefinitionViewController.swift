0178675bdd578ebe83a49d6678bccf9316aa0c2b
Ronald Huston Jr <rghuston9@gmail.com>
2020-04-14 10:26:47 -0500

    add functionality

--- Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
 
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var definitionField: UITextView!
+    
+    var vocabWord: VocabularyWord?
+    
+    func updateViews() {
+        if let vocabWord = vocabWord {
+            self.wordLabel.text = vocabWord.word
+            self.definitionField.text = vocabWord.definition
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateViews()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
