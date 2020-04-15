d8760bbfab913ab4db6eed6a324a536b5b5d0194
BrianRouse <brouse3815@me.com>
2020-04-14 22:04:07 -0400

    Updated File

---
+++ Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Brian Rouse on 4/13/20.
+//  Copyright © 2020 Brian Rouse. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord{
+    var word: String
+    var definition: String
+}
