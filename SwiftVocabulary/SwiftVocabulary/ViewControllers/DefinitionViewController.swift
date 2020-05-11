d9fb97d5e2fff7f91e39376754f2ed66daad8a63
violet-love <violetlove9419@icloud.com>
2020-05-11 14:23:21 -0700

    created view controller cocoa touch files, named the view controller scenes accordingly, and did the ui. Created outlets for the word label and definition text field

---
+++ SwiftVocabulary/SwiftVocabulary/ViewControllers/DefinitionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Violet Lavender Love on 5/11/20.
+//  Copyright © 2020 Love. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    @IBOutlet var WordLabel: UILabel!
+    @IBOutlet var DefinitionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
