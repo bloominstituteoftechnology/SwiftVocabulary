d227a530c33b3e5b45ed89f9d5ed0fd13fdabffd
dmills14 <drew-miller@lambdastudents.com>
2020-05-11 15:53:02 -0700

    segue problem

---
+++ Swift Vocabulary/Swift Vocabulary/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Swift Vocabulary
+//
+//  Created by Drew Miller on 5/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
