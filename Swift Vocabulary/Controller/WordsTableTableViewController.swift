29828007b49dd172de3336b5063385b83c58bb0b
wrscomputers <wrscomputers@gmail.com>
2020-05-11 18:33:36 -0400

    Commit 4

--- Swift Vocabulary/Controller/WordsTableTableViewController.swift
+++ Swift Vocabulary/Controller/WordsTableTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class WordsTableTableViewController: UITableViewController {
     
-    var vocabWords: [VocabularyWord] = []
+    var vocabWords: [VocabularyWord] = [VocabularyWord(word: "Variable",definition: "Provides us with named storage that our programs can manipulate. Each variable in Swift 4 has a specific type, which determines the size and layout of the variable's memory; the range of values that can be stored within that memory; and the set of operations that can be applied to the variable."),VocabularyWord(word: "Constant",definition: "Refers to fixed values that a program may not alter during its execution. Constants can be of any of the basic data types like an integer constant, a floating constant, a character constant, or a string literal."),VocabularyWord(word: "Function",definition: "A function is a set of statements organized together to perform a specific task.")]
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -18,9 +18,11 @@ class WordsTableTableViewController: UITableViewController {
     }
     
      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+     let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
      
-     // Configure the cell...
+     let vocabWord = vocabWords[indexPath.row]
+     
+     cell.textLabel?.text = vocabWord.word
      
      return cell
      }
