8fc9e251d4da4d194f22ef4a91731526114cc66d
Miguel Marquez <marquez53095@outlook.com>
2020-05-11 15:26:11 -0700

    Tenth revision, I created a new file called VocabularyWord.swift

---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,10 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Miguel Marquez on 5/11/20.
+//  Copyright © 2020 Miguel Marquez. All rights reserved.
+//
+
+import Foundation
+
