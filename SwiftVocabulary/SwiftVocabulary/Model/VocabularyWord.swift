567ba6465a4fa32dd35519c7360da4038fe75276
violet-love <violetlove9419@icloud.com>
2020-05-11 15:08:25 -0700

    created vocab word struct

---
+++ SwiftVocabulary/SwiftVocabulary/Model/VocabularyWord.swift
@@ -0,0 +1,16 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Violet Lavender Love on 5/11/20.
+//  Copyright © 2020 Love. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    
+    let word: String
+    let definition: String
+    
+}
