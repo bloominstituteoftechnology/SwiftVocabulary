f397966d41dc3daf0dc85ffa3d1d7c51f3bbf5df
Miguel Marquez <marquez53095@outlook.com>
2020-05-11 10:16:32 -0700

    Second revision, I created both a WordsTableViewController file and also a DefinitionViewController file

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Miguel Marquez on 5/11/20.
+//  Copyright © 2020 Miguel Marquez. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
