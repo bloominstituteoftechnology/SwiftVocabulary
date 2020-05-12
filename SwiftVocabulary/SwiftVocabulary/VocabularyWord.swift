57f2fd41201a52b46b36cf1a569f0ba4369a70c5
Miguel Marquez <marquez53095@outlook.com>
2020-05-11 17:10:16 -0700

    Eleventh revision, I created a VocabularyWord struct, added two strings, and then I initialized it

--- SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -8,3 +8,12 @@
 
 import Foundation
 
+struct VocabularyWord {
+    let word: String
+    let definition: String
+
+    init(word: String, definition: String) {
+        self.word = word
+        self.definition = definition
+    }
+}
