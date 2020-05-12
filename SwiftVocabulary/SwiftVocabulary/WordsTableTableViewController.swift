c613a41f4058f99b27db6d3f6f33cc7b9e59eaf0
Miguel Marquez <marquez53095@outlook.com>
2020-05-11 18:16:30 -0700

    Fifteenth revision, I set a segue.destination to DefinitionViewController

--- SwiftVocabulary/SwiftVocabulary/WordsTableTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/WordsTableTableViewController.swift
@@ -90,14 +90,17 @@ class WordsTableTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDefinitionSegue" {
+            let ShowDefinitionSegue = segue.destination
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
