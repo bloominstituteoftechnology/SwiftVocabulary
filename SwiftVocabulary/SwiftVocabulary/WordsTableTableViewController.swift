5d31c95a59b962e053522cd0c925f99828625a6d
Miguel Marquez <marquez53095@outlook.com>
2020-05-11 17:40:50 -0700

    fourteenth revision, I added a TableView CellForRowAt function

--- SwiftVocabulary/SwiftVocabulary/WordsTableTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/WordsTableTableViewController.swift
@@ -17,6 +17,11 @@ class WordsTableTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return vocabWords.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
