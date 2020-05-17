02c201b652db36803d6c8806a46b84baf2ae1a6f
johnmccants002 <johnmccants002@gmail.com>
2020-05-17 16:43:21 -0700

    added file

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by John McCants on 5/17/20.
+//  Copyright © 2020 John McCants. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var wordDefinition: UITextView!
+    
+    var vocabWord : VocabularyWord?
+    
+    func updateViews() {
+        
+        if let unwrappedVocabWord = vocabWord {
+            self.wordLabel.text = unwrappedVocabWord.word
+            self.wordDefinition.text = unwrappedVocabWord.definition
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       updateViews()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,18 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by John McCants on 5/17/20.
+//  Copyright © 2020 John McCants. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct VocabularyWord {
+    
+    var word : String
+    var definition : String
+    
+}
