18bc878e33d2ba6aa80c65d1f7c74386325391ee
Eugene White <eugenewhite1994@gmail.com>
2020-05-19 19:38:55 -0600

    Almost done

---
+++ Swift Vocab/Swift Vocab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Vocab
+//
+//  Created by Eugene White on 5/19/20.
+//  Copyright © 2020 Eugene White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift Vocab/Swift Vocab/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocab
+//
+//  Created by Eugene White on 5/19/20.
+//  Copyright © 2020 Eugene White. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
