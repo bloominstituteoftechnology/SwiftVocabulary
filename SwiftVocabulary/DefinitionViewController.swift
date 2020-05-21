8fc219f70b7c77d2c78d5c1970ffc78064522935
Sam Gutierrez <Samguti02@gmail.com>
2020-05-21 00:55:41 -0600

    Working on the last bullet point of step 5 under Implement WordsTableViewController

--- SwiftVocabulary/DefinitionViewController.swift
+++ SwiftVocabulary/DefinitionViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
-
+    
+    var vocabWord: VocabularyWord?
+    
+func updateViews() {
+        if let unwrappedVocabWord = vocabWord {
+            unwrappedVocabWord.word
+            unwrappedVocabWord.definition
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
--- SwiftVocabulary/WordsTableViewController.swift
+++ SwiftVocabulary/WordsTableViewController.swift
@@ -36,14 +36,19 @@ class WordsTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
+        if segue.identifier == "ShowDefinitionSegue" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let definitionVC = segue.destination as? DefinitionViewController
+                
+                definitionVC?.vocabWord = vocabWords[indexPath.row].self
+            }
+            
+        }
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
