0ead193809d05d57d68b6ed4e1e2055904bff6fb
Ben James <bejjmail@gmail.com>
2020-05-20 22:23:01 -0700

    I think i'm done just getting a build erorr i'm trying to figure out

---
+++ navigationControllersGuidedProject/navigationControllersGuidedProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  navigationControllersGuidedProject
+//
+//  Created by Dojo on 5/19/20.
+//  Copyright © 2020 Dojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
