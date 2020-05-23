8dd1f37ec505131595b0c6e4ad9df69cc07397ac
Brys1Saclaus2 <brys1saclaus2@gmail.com>
2020-05-23 10:43:10 -0700

    2nd push FIXED SEGUEE

--- swiftVocabularyProject/DefinitionViewController.swift
+++ swiftVocabularyProject/DefinitionViewController.swift
@@ -33,14 +33,22 @@ class DefinitionViewController: UIViewController {
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
+        
+    
+        
+     
+     
+     // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
+
+
+
