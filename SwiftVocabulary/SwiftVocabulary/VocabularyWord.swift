7b30a65a7706d09486d83b9fd5155ef353fcbba9
James McDougall <jameswilliammcdougall@gmail.com>
2020-12-06 11:52:48 -0600

    Added VocabularyWord.swift file andd struct

---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,13 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by James McDougall on 12/6/20.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
