2b5eaee62adde712334aea57164ce4b0ef8dfaf7
James McDougall <jameswilliammcdougall@gmail.com>
2020-12-06 11:57:04 -0600

    Created vocabWords array of VocabularyWord objects

--- SwiftVocabulary/SwiftVocabulary/WordsTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/WordsTableViewController.swift
@@ -8,15 +8,14 @@
 import UIKit
 
 class WordsTableViewController: UITableViewController {
+    
+    var vocabWords: [VocabularyWord] = [VocabularyWord(word: "Variable", definition: ""),
+                                        VocabularyWord(word: "Constant", definition: ""),
+                                        VocabularyWord(word: "Function", definition: "")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
