58ad5135d3f323c817637694655f6bc88b71bbb9
James McDougall <jameswilliammcdougall@gmail.com>
2020-12-06 12:28:19 -0600

    MVP

--- SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -8,13 +8,21 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
+    
+    var vocabWord: VocabularyWord?
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var definitionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
+    }
+    
+    func updateViews() {
+        guard let cellContents = vocabWord else { return }
+        titleLabel.text = cellContents.word
+        definitionTextView.text = cellContents.definition
     }
 
 }
--- SwiftVocabulary/SwiftVocabulary/WordsTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/WordsTableViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class WordsTableViewController: UITableViewController {
     
-    var vocabWords: [VocabularyWord] = [VocabularyWord(word: "Variable", definition: ""),
-                                        VocabularyWord(word: "Constant", definition: ""),
-                                        VocabularyWord(word: "Function", definition: "")]
+    let wordCell: String = "WordCell"
+    
+    var vocabWords: [VocabularyWord] = [VocabularyWord(word: "Variable", definition: "A named value used to store information. Variables can be changed after being created"),
+                                        VocabularyWord(word: "Constant", definition: "A named value used to store information. Constants can NOT be changed after being created."),
+                                        VocabularyWord(word: "Function", definition: "An object that can be used to do work with an application. This can be modified to do anything that you want it to.")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,68 +23,29 @@ class WordsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return vocabWords.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: wordCell, for: indexPath)
+        let myWord = vocabWords[indexPath.row]
+        cell.textLabel?.text = myWord.word
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let indexPath = tableView.indexPathForSelectedRow,
+              let definitionDetailVC = segue.destination as? DefinitionViewController else { return }
+        
+        definitionDetailVC.vocabWord = vocabWords[indexPath.row]
     }
-    */
 
 }
