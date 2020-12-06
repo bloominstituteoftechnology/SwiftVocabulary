b31b0ddc4cffd125d90347f2ad54baa4f35b576c
James McDougall <jameswilliammcdougall@gmail.com>
2020-12-06 11:51:26 -0600

    Project setup completed

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by James McDougall on 12/6/20.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var definitionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
