278d0a3a150bb2252798feed761fb5ac044fe6a9
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-12-19 08:16:32 -0800

    Improved error handling, removed unnecessary optionality

--- Swift Vocabulary/Swift Vocabulary/Controller/WordsTableViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/Controller/WordsTableViewController.swift
@@ -14,32 +14,23 @@ class WordsTableViewController: UITableViewController {
         addWord()
     }
     //MARK: Class Properties
-    var vocabWords: [VocabularyWord]?
+    var vocabWords: [VocabularyWord] = VocabularyWord.mockWords
     
     
     //MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.vocabWords = [
-            VocabularyWord(word: "var",
-                           definition: "In Swift, a variable is a mutable property."),
-            VocabularyWord(word: "let",
-                           definition: "In Swift, a let is an immutable property, also known as a constant."),
-            VocabularyWord(word: "optional",
-                           definition: "Think of a box. You don't know what's in it until you unwrap it. An optional is a property with a value that may be nil (nothing).")
-        ]
-        
         self.tableView.reloadData()
     }
     
     //MARK: TableView Delegate and DataSource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return vocabWords?.count ?? 0 //return vocabwords.count. if vocabWords is nil, return 0
+        return vocabWords.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell") else { return UITableViewCell() }
-        guard let word = self.vocabWords?[indexPath.row] else { return UITableViewCell() }
+        let word = self.vocabWords[indexPath.row]
         cell.backgroundColor = .systemTeal
         cell.textLabel?.textColor = .systemBackground
         cell.textLabel?.text = word.word
@@ -50,10 +41,16 @@ class WordsTableViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowDefinitionSegue" {
-            guard let destination = segue.destination as? DefinitionViewController else { genericError(); return } //could write on 2 lines without the semicolon
-            guard let row = tableView.indexPathForSelectedRow?.row else { genericError(); return }
-            //to test genericError(), use vocabWords = nil
-            guard let word = vocabWords?[row] else { genericError(); return }
+            guard let destination = segue.destination as? DefinitionViewController else {
+                genericError()
+                return
+            }
+            guard let row = tableView.indexPathForSelectedRow?.row else {
+                genericError()
+                return
+            }
+
+            let word = vocabWords[row]
             destination.vocabWord = word
         }
     }
@@ -61,13 +58,17 @@ class WordsTableViewController: UITableViewController {
     //MARK: Helper Methods
     
     func addWord() {
-        Alert.instance.showWithInput(title: "Enter New Word", message: "Enter a Word and Definition", vc: self) { (word) in
-            if word.word != "Enter Word" && word.definition != "Enter Definition" && word.word != "" && word.definition != "" { //if a word and definition were entered:
-                self.vocabWords?.append(word)
-                self.tableView.reloadData()
-            } else {
-                Alert.instance.show(title: "Word Not Entered", message: "Please Try Again", vc: self)
+        Alert.instance.showWithInput(title: "Enter New Word", message: "Enter a Word and Definition", vc: self) { (entry) in
+            if entry.word == "Enter Word" || entry.word.isEmpty {
+                Alert.instance.show(title: "Word Not Entered", message: "Please enter the word you wish to define", vc: self)
             }
+            if entry.definition == "Enter Definition" || entry.definition.isEmpty {
+                Alert.instance.show(title: "Definition Not Entered", message: "Please define the word you're entering", vc: self)
+            } else {
+                self.vocabWords.append(entry)
+                self.tableView.reloadData()
+            }
+
         }
     }
     
--- Swift Vocabulary/Swift Vocabulary/Model/VocabularyWord.swift
+++ Swift Vocabulary/Swift Vocabulary/Model/VocabularyWord.swift
@@ -11,4 +11,13 @@ import Foundation
 struct VocabularyWord {
     var word: String
     var definition: String
+
+    static let mockWords: [VocabularyWord] = [
+        VocabularyWord(word: "var",
+                       definition: "In Swift, a variable is a mutable property."),
+        VocabularyWord(word: "let",
+                       definition: "In Swift, a let is an immutable property, also known as a constant."),
+        VocabularyWord(word: "optional",
+                       definition: "Think of a box. You don't know what's in it until you unwrap it. An optional is a property with a value that may be nil (nothing).")
+    ]
 }
