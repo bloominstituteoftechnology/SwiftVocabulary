30c4192cb7874793db2504bfdc47bc1ebb365b80
Sammy Alvarado <isammyalvarado@gmail.com>
2021-02-24 17:11:06 -0500

    Added initial Xcode project!

---
+++ SwiftVocabulary/SwiftVocabulary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Sammy Alvarado on 2/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
