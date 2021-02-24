e30ff7e2e5a46a6df2db1da2221b1f590b1b9462
Sammy Alvarado <isammyalvarado@gmail.com>
2021-02-24 17:36:34 -0500

    Cleaned up the Xcode project and created new folder to hold in view controllers. Resigned the plist to a new folder.

---
+++ SwiftVocabulary/SwiftVocabulary/View Controller/DefintionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DefintionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Sammy Alvarado on 2/24/21.
+//
+
+import UIKit
+
+class DefintionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
