cb07ed2e3ffc6ece8f003aead98a670ba36510e0
Waseem Idelbi <idelbiw@gmail.com>
2022-05-12 13:26:39 -0400

    Created the project, deleted the default screen in the main storyboard, and created the 2 files for the words table view and the definition view controller

---
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Waseem Idelbi on 5/12/22.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
