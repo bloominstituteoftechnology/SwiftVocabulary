3c5a957cc3f1efc353101b0e6596da07832698cf
Waseem Idelbi <idelbiw@gmail.com>
2022-05-12 22:27:36 -0400

    created a file for the model with a struct for it inside

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,13 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Waseem Idelbi on 5/12/22.
+//
+
+import Foundation
+
+struct VocabularyWord: Codable {
+    let word: String
+    let definition: String
+}
